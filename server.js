1668b061df138a57b9ea76cb220bbe190ed57689
Israel <israeltademola@gmail.com>
2019-05-02 19:01:29 +0100

    add heroku procfile

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ mongoose.connect(env.mongodb_url).then(() => {
 });
 
 app.use(cors());
+
+app.use((req, res, next)=>{
+  console.log(`$[${new Date().toTimeString}]: ${reg.method} ${req.url}`);
+
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
