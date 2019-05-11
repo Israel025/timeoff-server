5be89eaef76e7138f2746a5502fb1e667ab781a1
Israel <israeltademola@gmail.com>
2019-05-11 09:24:57 +0100

    cors fix 9

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(cors({
   'allowedHeaders': ['sessionId', 'Content-Type'],
   'exposedHeaders': ['sessionId'],
-  'origin': 'https://izzy-timeoff.herokuapp.com',
+  'origin': true,
   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
   'preflightContinue': false
 }));
@@ -36,11 +36,18 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/user", UserRoute);
 
-
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'origin': true,
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}));
 app.use("/leave", LeaveRequestRoute);
 
 app.listen(port).on("listening", () => {
